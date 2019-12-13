2a4ccbc26a27cf3f035f05e4003fd476dedd6064
Raghu Betina <raghu@firstdraft.com>
2019-12-13 03:08:46 +0000

    Only milk user can access show page

--- app/controllers/milks_controller.rb
+++ app/controllers/milks_controller.rb
@@ -1,5 +1,5 @@
 class MilksController < ApplicationController
-  before_action :current_user_must_be_milk_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_milk_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_milk_user
     milk = Milk.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
