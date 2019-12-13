79f0d3a6459934181d26e5722c0eeb156ec3b03d
Raghu Betina <raghu@firstdraft.com>
2019-12-13 03:08:46 +0000

    Whitelist columns in Milk for ActiveAdmin

--- app/admin/milks.rb
+++ app/admin/milks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Milk do
+
+ permit_params :user_id, :date_milk_expires, :description
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
